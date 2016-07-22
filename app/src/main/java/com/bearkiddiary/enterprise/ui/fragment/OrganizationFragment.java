121081ca224812d46e22d01ba056cfb1f7894bc0
SCNUzy <975135274@qq.com>
2016-07-22 11:53:55 +0800

    初始化目录

--- app/src/main/java/com/bearkiddiary/enterprise/ui/fragment/OrganizationFragment.java
+++ app/src/main/java/com/bearkiddiary/enterprise/ui/fragment/OrganizationFragment.java
@@ -5,12 +5,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bearkiddiary.enterprise.R;
+
 /**
  * Created by admin on 2016/7/22.
  */
-public class OrganizationFragment extends BaseFragment{
+public class OrganizationFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_organization, container, false);
+        initView(view);
+        return view;
+    }
+
+    private final void initView(View view){
+
     }
 }
--- app/src/main/java/com/bearkiddiary/enterprise/ui/view/initView.java
+++
@@ -1,7 +0,0 @@
-package com.bearkiddiary.enterprise.ui.view;
-
-/**
- * Created by admin on 2016/7/22.
- */
-public class initView {
-}
