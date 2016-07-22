b4ddcca7efc4c864922e7a4b1acf917261e24a01
SCNUzy <975135274@qq.com>
2016-07-22 11:13:54 +0800

    初始化目录

--- app/src/main/java/com/bearkiddiary/enterprise/ui/fragment/OrganizationFragment.java
+++ app/src/main/java/com/bearkiddiary/enterprise/ui/fragment/OrganizationFragment.java
@@ -1,8 +1,16 @@
 package com.bearkiddiary.enterprise.ui.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by admin on 2016/7/22.
  */
 public class OrganizationFragment extends BaseFragment{
-
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
